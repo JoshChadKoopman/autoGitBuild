535fe88f01b146a8f97a62f5b4077b6f77797ddf
Josh Koopman <koopmanmac@Joshs-MacBook-Air.local>
2025-09-23 09:42:16 +0200

    changed Jenkins token

--- jenkins-trigger.sh
+++ jenkins-trigger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-JENKINS_JOB_URL="http://localhost:8085/job/AutoGitBuild/build?token=${JENKINS_TOKEN}"
+JENKINS_JOB_URL="http://jenkins:8085/job/AutoGitBuild/build?token=${JENKINS_TOKEN}"
 curl -X POST "$JENKINS_JOB_URL"
